7419be00afa6a0ef4c12977b605cf6faed84fe60
Andrejalisson <andrejalisson@gmail.com>
2025-06-05 16:37:28 -0300

    Add Metronic-based authentication pages

--- resources/views/metronic/layout.blade.php
+++ resources/views/metronic/layout.blade.php
@@ -1,1 +1,21 @@
-<!-- Metronic layout placeholder -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Metronic Global Stylesheets Bundle -->
+    <link rel="stylesheet" href="{{ asset('metronic/assets/plugins/global/plugins.bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('metronic/assets/css/style.bundle.css') }}">
+</head>
+<body id="kt_body" class="app-blank">
+    {{ $slot }}
+
+    <!-- Metronic Global Javascript Bundle -->
+    <script src="{{ asset('metronic/assets/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('metronic/assets/js/scripts.bundle.js') }}"></script>
+
+    @stack('scripts')
+</body>
+</html>
