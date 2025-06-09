580f51cfd07da5d0657691ef9e94caf1e79763b6
Andrejalisson <andrejalisson@gmail.com>
2025-06-09 15:59:36 -0300

    Add AJAX modal for creating users

---
+++ resources/views/users/partials/create-modal.blade.php
@@ -0,0 +1,34 @@
+<div class="modal-header">
+    <h5 class="modal-title">Novo Usuário</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<form id="createUserForm" method="POST" action="{{ route('users.store') }}">
+    @csrf
+    <div class="modal-body">
+        <div class="mb-3">
+            <input class="form-control" type="text" name="name" value="{{ old('name') }}" required placeholder="Nome" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="email" name="email" value="{{ old('email') }}" required placeholder="E-mail" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="password" name="password" required placeholder="Senha" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <select name="role_id" class="form-select">
+                <option value="">Selecione um grupo</option>
+                @foreach($roles as $role)
+                    <option value="{{ $role->id }}" @selected(old('role_id') == $role->id)>{{ $role->name }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('role_id')" class="mt-2" />
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </div>
+</form>
